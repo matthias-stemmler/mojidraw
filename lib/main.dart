88e55d43ad28f458675c8677da01525312369970
Matthias Stemmler <matthias.stemmler@gmail.com>
2021-02-11 22:23:19 +0100

    Switch to canvas rendering

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,37 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MojiDrawApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MojiDrawApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Mojidraw',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Mojidraw', width: 11, height: 11),
+      home: MojiDrawPage(title: 'Mojidraw', width: 15, height: 15),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title, this.width, this.height}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+class MojiDrawPage extends StatefulWidget {
   final String title;
   final int width, height;
 
+  MojiDrawPage({Key key, this.title, this.width, this.height})
+      : super(key: key);
+
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MojiDrawPageState createState() => _MojiDrawPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MojiDrawPageState extends State<MojiDrawPage> {
   _Emojis _emojis;
 
   @override
@@ -59,9 +40,9 @@ class _MyHomePageState extends State<MyHomePage> {
     _emojis = _Emojis(widget.width, widget.height);
   }
 
-  void _toggleEmoji(int x, int y) {
+  void _activateEmoji(int x, int y) {
     setState(() {
-      _emojis.toggle(x, y);
+      _emojis.activate(x, y);
     });
   }
 
@@ -71,21 +52,9 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-          child: SingleChildScrollView(
-              padding: EdgeInsets.all(20.0),
-              child: Table(
-                  border: TableBorder.all(color: Colors.grey),
-                  children: List.generate(
-                      widget.height,
-                      (y) => TableRow(
-                          children: List.generate(
-                              widget.width,
-                              (x) => _GridCell(
-                                  active: _emojis.isActive(x, y),
-                                  onTap: () {
-                                    _toggleEmoji(x, y);
-                                  }))))))),
+      body: Container(
+          padding: EdgeInsets.all(20.0),
+          child: EmojiGrid(emojis: _emojis, onEmojiTouch: _activateEmoji)),
       floatingActionButton: FloatingActionButton(
         tooltip: 'Increment',
         child: Icon(Icons.add),
@@ -94,39 +63,140 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class _GridCell extends StatelessWidget {
-  final bool active;
-  final GestureTapCallback onTap;
+class EmojiGrid extends StatelessWidget {
+  final _Emojis emojis;
+  final void Function(int x, int y) onEmojiTouch;
 
-  const _GridCell({Key key, this.active, this.onTap}) : super(key: key);
+  const EmojiGrid({Key key, this.emojis, this.onEmojiTouch}) : super(key: key);
+
+  _handlePanUpdate(Offset position, Size size) {
+    final layout = GridLayout(size, emojis.width, emojis.height);
+    final cell = layout.offsetToCell(position);
+
+    if (cell != null) {
+      onEmojiTouch(cell.x, cell.y);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-        onTap: onTap,
-        child: Container(
-            decoration: BoxDecoration(),
-            child: AspectRatio(
-                aspectRatio: 1.0,
-                child: Center(
-                    child:
-                        Text(active ? '🦔️' : '❤', textScaleFactor: 1.5)))));
+        onPanUpdate: onEmojiTouch == null
+            ? null
+            : (details) {
+                _handlePanUpdate(details.localPosition, context.size);
+              },
+        child: CustomPaint(painter: GridPainter(emojis), size: Size.infinite));
+  }
+}
+
+class GridPainter extends CustomPainter {
+  final _Emojis _emojis;
+  final TextEmoji _heart = TextEmoji('❤');
+  final TextEmoji _hedgehog = TextEmoji('🦔');
+
+  GridPainter(this._emojis);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final layout = GridLayout(size, _emojis.width, _emojis.height);
+    final cellWidth = layout.cellSize.width;
+    final heart = _heart.render(cellWidth);
+    final hedgehog = _hedgehog.render(cellWidth);
+
+    for (final y in Iterable<int>.generate(_emojis.height)) {
+      for (final x in Iterable<int>.generate(_emojis.width)) {
+        canvas.drawParagraph(this._emojis.isActive(x, y) ? hedgehog : heart,
+            layout.cellToOffset(GridCell(x, y)));
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
+}
+
+class GridLayout {
+  Offset _offset;
+  Size _cellSize;
+  int _horizontalCells, _verticalCells;
+
+  GridLayout(Size size, this._horizontalCells, this._verticalCells) {
+    // TODO support non-square grids
+    final length = size.shortestSide;
+    _offset = Offset((size.width - length) / 2, (size.height - length) / 2);
+    _cellSize = Size(length / _horizontalCells, length / _verticalCells);
+  }
+
+  Offset cellToOffset(GridCell cell) =>
+      _offset + Offset(cell.x * _cellSize.width, cell.y * _cellSize.height);
+
+  GridCell offsetToCell(Offset offset) {
+    final localOffset = offset - _offset;
+    int x = (localOffset.dx / _cellSize.width).floor();
+    int y = (localOffset.dy / _cellSize.height).floor();
+
+    return x >= 0 && x < _horizontalCells && y >= 0 && y < _verticalCells
+        ? GridCell(x, y)
+        : null;
+  }
+
+  Size get cellSize => _cellSize;
+}
+
+class GridCell {
+  final int x, y;
+
+  GridCell(this.x, this.y);
+}
+
+class TextEmoji {
+  final String _text;
+  double _widthFactor;
+
+  TextEmoji(this._text) {
+    _widthFactor = _getWidthFactor();
+  }
+
+  Paragraph render(double width) {
+    final builder =
+        ParagraphBuilder(ParagraphStyle(fontSize: width * _widthFactor));
+    builder.addText(_text);
+    final paragraph = builder.build();
+    paragraph.layout(ParagraphConstraints(width: width));
+
+    return paragraph;
+  }
+
+  double _getWidthFactor() {
+    final textStyle = TextStyle(fontSize: 14.0);
+    final textPainter = TextPainter(
+        textDirection: TextDirection.ltr,
+        text: TextSpan(text: _text, style: textStyle));
+    textPainter.layout();
+
+    return textStyle.fontSize / textPainter.width;
   }
 }
 
 class _Emojis {
-  final int _width;
+  final int _width, _height;
   final List<bool> _states;
 
-  _Emojis(this._width, height) : _states = List.filled(_width * height, false);
+  _Emojis(this._width, this._height)
+      : _states = List.filled(_width * _height, false);
+
+  int get width => _width;
+
+  int get height => _height;
 
   bool isActive(int x, int y) {
     return _states[getIndex(x, y)];
   }
 
-  void toggle(int x, int y) {
+  void activate(int x, int y) {
     var index = getIndex(x, y);
-    _states[index] = !_states[index];
+    _states[index] = true;
   }
 
   int getIndex(int x, int y) => y * _width + x;
