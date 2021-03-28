b52d5b4ed9792fb68039d9fa3768916909105ddf
Matthias Stemmler <matthias.stemmler@gmail.com>
2021-03-28 21:16:42 +0200

    Recreate Flutter project and switch from Java to Kotlin

--- android/app/src/main/java/app/mojidraw/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package app.mojidraw;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
---
+++ android/app/src/main/kotlin/app/mojidraw/MainActivity.kt
@@ -0,0 +1,6 @@
+package app.mojidraw
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
