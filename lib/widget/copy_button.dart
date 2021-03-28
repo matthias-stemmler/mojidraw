63f388aa9a000c5696cab0884bbd865789054228
Matthias Stemmler <matthias.stemmler@gmail.com>
2021-03-28 21:29:35 +0200

    Make method private

--- lib/widget/copy_button.dart
+++ lib/widget/copy_button.dart
@@ -7,14 +7,14 @@ import '../state/drawing_state.dart';
 
 @immutable
 class CopyButton extends StatelessWidget {
-  void copy(String text) => Clipboard.setData(ClipboardData(text: text));
+  void _copy(String text) => Clipboard.setData(ClipboardData(text: text));
 
   @override
   Widget build(BuildContext context) => IconButton(
       icon: const Icon(Icons.copy),
       tooltip: 'Copy to clipboard',
       onPressed: () {
-        copy(context.read<DrawingState>().gridAsText);
+        _copy(context.read<DrawingState>().gridAsText);
 
         Fluttertoast.showToast(
           msg: 'Copied to clipboard',
