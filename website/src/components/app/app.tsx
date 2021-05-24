0e4d3ff134587a2d83f1e1aa5a6ffed4d87d78aa
Matthias Stemmler <matthias.stemmler@gmail.com>
2021-05-24 17:26:27 +0200

    Fix footer

--- website/src/components/app/app.tsx
+++ website/src/components/app/app.tsx
@@ -1,17 +1,32 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect, useRef } from 'react';
 import Footer from '../footer/footer';
 import Header from '../header/header';
 import Main from '../main/main';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-const App = (): ReactElement => (
-  <div className="d-flex flex-column h-100">
-    <Router>
-      <Header />
-      <Main />
-      <Footer />
-    </Router>
-  </div>
-);
+const App = (): ReactElement => {
+  const appRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleResize = (): void => {
+      appRef.current!.style.height = `${window.innerHeight}px`;
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, [appRef]);
+
+  return (
+    <div className="d-flex flex-column" ref={appRef}>
+      <Router>
+        <Header />
+        <Main />
+        <Footer />
+      </Router>
+    </div>
+  );
+};
 
 export default App;
